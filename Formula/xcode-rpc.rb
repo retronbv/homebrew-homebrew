7916b75fa2031d800c96f926ac0feb8b1250a1cb
retronbv <me@retron.dev>
2024-10-15 20:49:01 -0400

    im going to murder vscode

--- Formula/xcode-rpc.rb
+++ Formula/xcode-rpc.rb
@@ -3,6 +3,7 @@ class XcodeRpc < Formula
   homepage "https://github.com/retronbv/xcode-rpc"
   url "https://github.com/retronbv/xcode-rpc/releases/latest/download/xcode-rpc.tar.gz"
   sha256 "4882855754cd9138c19201d2f33dbdeff40e660352834590ac30332f04938fb5"
+  version "1.0.0"
 
   def install
     bin.install "xcode-rpc"
