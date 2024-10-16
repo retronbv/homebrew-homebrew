8aa22b401b97c075e2dd8239c0bf77b42827dc6b
retronbv <me@retron.dev>
2024-10-15 20:45:09 -0400

    bruh

---
+++ Formula/xcode-rpc.rb
@@ -0,0 +1,17 @@
+class Xcoderpc < Formula
+  desc "Discord Rich Presence client for Xcode"
+  homepage "https://github.com/retronbv/xcode-rpc"
+  url "https://github.com/retronbv/xcode-rpc/releases/latest/download/xcode-rpc.tar.gz"
+  sha256 "4882855754cd9138c19201d2f33dbdeff40e660352834590ac30332f04938fb5"
+
+  def install
+    bin.install "xcode-rpc"
+  end
+
+  service do
+    run opt_bin/"xcode-rpc"
+    keep_alive true
+    working_dar var/"xcode-rpc"
+    error_log_path var/"xcode-rpc/discord-rpc.log"
+  end
+end
